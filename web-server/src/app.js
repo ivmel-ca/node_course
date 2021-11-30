90b938842690d586fc407b2974fd1196521f2e19
Ivan Melnychenko <ivmel.ca@gmail.com>
2021-11-30 12:33:04 +0200

    feat: add handlebars to template html files serving

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,34 @@
 const path = require("path");
 const express = require("express");
 
-console.log("__dirname: ", __dirname);
-console.log(path.join(__dirname, "../public"));
-
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+// enable handlebars to serve dynamic templates
+app.set("view engine", "hbs");
+// serve static files
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Ivan Melnychenko"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About page",
+    name: "Ivan Melnychenko"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    name: "Ivan Melnychenko"
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send(JSON.stringify({ temperature: "-20", location: "Calgary" }));
