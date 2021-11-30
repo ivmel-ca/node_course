3b354642dc1441fff3589c0b4865092e57a451cd
Ivan Melnychenko <ivmel.ca@gmail.com>
2021-11-30 11:53:45 +0200

    feat: serve static html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,18 @@
 const path = require("path");
 const express = require("express");
 
-console.log(__dirname);
+console.log("__dirname: ", __dirname);
 console.log(path.join(__dirname, "../public"));
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.use(express.static);
-
-app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send("Help page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>about page</h1>");
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
   res.send(JSON.stringify({ temperature: "-20", location: "Calgary" }));
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(3002, () => {
+  console.log("Server is up on port 3002");
 });
