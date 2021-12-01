6f0c592718edc5997774ab61d321ec5385f723d1
Ivan Melnychenko <ivmel.ca@gmail.com>
2021-12-01 16:24:32 +0200

    feat: fetch forecast data from backend to frontend

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,6 @@
 console.log("client side javascript file is loaded");
+
+fetch('http://localhost:3002/weather?city="kitchener"')
+  .then((resp) => resp.json())
+  .then((resp) => console.log(resp))
+  .catch((err) => console.error(err));
