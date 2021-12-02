c77f5b28479e9c9e6b09d3c445b8834ca74c6c11
Ivan Melnychenko <ivmel.ca@gmail.com>
2021-12-02 18:11:27 +0200

    feat: add form for forecast search, customize it

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,33 @@
-console.log("client side javascript file is loaded");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-fetch('http://localhost:3002/weather?city="kitchener"')
-  .then((resp) => resp.json())
-  .then((resp) => console.log(resp))
-  .catch((err) => console.error(err));
+if (weatherForm) {
+  weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const city = search.value;
+
+    fetch(`http://localhost:3002/weather?city=${city}`)
+      .then((resp) => resp.json())
+      .then(
+        ({
+          location,
+          forecastData: { temperature, feelslike } = {},
+          error
+        }) => {
+          if (error) {
+            return (messageOne.textContent = error);
+          }
+
+          messageOne.textContent = "";
+
+          const msg = `Location: ${location}.
+          The temperature is ${temperature}, it feels like ${feelslike}`;
+
+          messageTwo.textContent = msg;
+        }
+      )
+      .catch((err) => console.error(err));
+  });
+}
