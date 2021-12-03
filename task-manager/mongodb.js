6ebb4af03ce53eadf3e19d49b0c2e811b6d0cf43
Ivan Melnychenko <ivmel.ca@gmail.com>
2021-12-03 10:53:10 +0200

    feat: init the task-manager app. Connect to a local mongodb and insert initial data

---
+++ task-manager/mongodb.js
@@ -0,0 +1,27 @@
+// CRUD - create read update delete
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+// do not use localhost, or else expect unpredicted behavior
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  /* by default the old URL parser is deprecated,
+   the new one should be called explicitly */
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.error("Unable to connect to the database!");
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection("users").insertOne({
+      name: "Ivan",
+      age: "24"
+    });
+  }
+);
