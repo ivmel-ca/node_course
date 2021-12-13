767ef5aed09320b00b4b9a77faa2fffd6cbf5b07
Ivan Melnychenko <ivmel.ca@gmail.com>
2021-12-13 18:23:55 +0200

    feat: move task model into a separate file

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+    trim: true
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be grater than 0");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password should not contain password!");
+      }
+    }
+  }
+});
+
+module.exports = User;
